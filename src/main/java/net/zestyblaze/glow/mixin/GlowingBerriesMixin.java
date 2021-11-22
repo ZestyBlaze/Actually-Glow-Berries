43d9961fc83f60e07283c1a173661e6b0c7d82d8
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2021-11-22 01:19:57 +0000

    1.0.2
    - Updated to 1.18 pre-release 5
    - Bumped Loom
    - Reduced file size
    - You can shift right click glow berries to apply glowing to other mobs!
    - Updated to Java 17

--- src/main/java/net/zestyblaze/glow/mixin/GlowingBerriesMixin.java
+++ src/main/java/net/zestyblaze/glow/mixin/GlowingBerriesMixin.java
@@ -3,9 +3,12 @@ package net.zestyblaze.glow.mixin;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,6 +22,19 @@ public class GlowingBerriesMixin {
         if(!world.isClient) {
             if (stack.getItem() == Items.GLOW_BERRIES) {
                 user.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 100, 0, true, false));
+            }
+        }
+    }
+    @Inject(method = "useOnEntity", at = @At("HEAD"))
+    private void glowBerryOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        if(!entity.world.isClient) {
+            if(stack.getItem() == Items.GLOW_BERRIES && !(entity instanceof PlayerEntity)) {
+                if(user.isSneaking() && !entity.hasStatusEffect(StatusEffects.GLOWING)) {
+                    if (!user.getAbilities().creativeMode) {
+                        stack.decrement(1);
+                    }
+                    entity.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 600, 0, true, true));
+                }
             }
         }
     }
