9fd04cc4a6a494b9a98150f36680d46ddd25f5c3
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2021-11-22 00:36:04 +0000

    1.0.2
    - Bumped mod version
    - Removed main entrypoint as it was unused
    - Changed MavenPublication for new Loom version
    - Bumped Yarn version
    
    - You can now use Glow Berries to give Glowing to other entities!

--- src/main/java/net/zestyblaze/glow/mixin/GlowingBerriesMixin.java
+++ src/main/java/net/zestyblaze/glow/mixin/GlowingBerriesMixin.java
@@ -3,11 +3,13 @@ package net.zestyblaze.glow.mixin;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
-import net.zestyblaze.glow.GlowBerries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,11 +17,24 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Item.class)
 public class GlowingBerriesMixin {
-    @Inject(method = "finishUsing", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "finishUsing", at = @At("HEAD"))
     private void glowBerryGlowEffect(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir) {
         if(!world.isClient) {
             if (stack.getItem() == Items.GLOW_BERRIES) {
-                user.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 100, 0, true, false));
+                user.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 600, 0, true, true));
+            }
+        }
+    }
+    @Inject(method = "useOnEntity", at = @At("HEAD"))
+    private void glowBerryOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        if(!entity.world.isClient) {
+            if(stack.getItem() == Items.GLOW_BERRIES && !(entity instanceof PlayerEntity)) {
+                if(user.isSneaking() && !entity.hasStatusEffect(StatusEffects.GLOWING)) {
+                    if (!user.getAbilities().creativeMode) {
+                        stack.decrement(1);
+                    }
+                    entity.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 600, 0, true, true));
+                }
             }
         }
     }
